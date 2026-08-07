dbbf96bfdcd9ce612bacff6de04efc68505f85c0
dboghra <boghra.d@northeastern.edu>
2026-08-06 23:46:39 -0400

    Initial structure and comments

---
+++ onboard.py
@@ -0,0 +1,6 @@
+#load .env
+#load config file(represents each team)
+# extract data from config(repo name, team name, members, etc)
+#validate config data
+#call provisioner to create repo and add members
+
