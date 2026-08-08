8d3ac5623ea23df303fb45559e77a260fc017e07
dboghra <boghra.d@northeastern.edu>
2026-08-07 23:57:57 -0400

    flush out provisioner functionality

---
+++ provisioner.py
@@ -0,0 +1,15 @@
+
+
+#call github api to create repo 
+    # use repo_name from the row
+    #use description from the row
+# add members to repo
+    #use github_username form the row
+    #use member roles from the row to set permisions
+        #admin --> admin access
+        #developer --> push access
+# set the branch protections on main so required reviews and no force push
+
+
+
+
