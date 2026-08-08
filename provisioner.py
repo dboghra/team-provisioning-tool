609062a32f524b6715b4941eb6d79f427d4b3363
dboghra <boghra.d@northeastern.edu>
2026-08-08 00:01:05 -0400

    add function stubs to provisioner.py

--- provisioner.py
+++ provisioner.py
@@ -11,6 +11,28 @@
 # set the branch protections on main so required reviews and no force push
 #commit the run template to the repo(run-test.yml) and set it to run on push 
 
+def create_repo(org, repo_name, description, token):
+    # Create repo on GitHub
+    # Return success or failure
+
+def add_member(org, repo_name, github_username, member_role, token):
+    # Add member to repo with correct permission
+    # Return success or failure
+
+def set_branch_protections(org, repo_name, token):
+    # Set protections on main branch
+    # Return success or failure
+
+def commit_template(org, repo_name, template_path, token):
+    # Commit run-test.yml to repo
+    # Return success or failure
+
+def provision_all_teams(valid_rows, token, org):
+    # Main function that orchestrates everything
+    # Keeps track of which repos are done
+    # Calls the above functions in the right order
+    # Returns successes and failures list
+
 
 
 
