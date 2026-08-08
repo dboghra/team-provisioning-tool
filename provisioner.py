429c0b0be688ad721f62c36bf223134e57afd972
dboghra <boghra.d@northeastern.edu>
2026-08-07 23:58:13 -0400

    more

--- provisioner.py
+++ provisioner.py
@@ -4,11 +4,12 @@
     # use repo_name from the row
     #use description from the row
 # add members to repo
-    #use github_username form the row
-    #use member roles from the row to set permisions
+    #use github_username form the row and check if it exists, if not create it
+    #add members to the repo and use member roles from the row to set permisions
         #admin --> admin access
         #developer --> push access
 # set the branch protections on main so required reviews and no force push
+#commit the run template to the repo(run-test.yml) and set it to run on push 
 
 
 
